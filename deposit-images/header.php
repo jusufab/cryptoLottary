02bc4ad1fa36ac387296b9a820ed94000b29f8da
Jusuf <53156085+jusufab@users.noreply.github.com>
2022-06-07 22:18:16 +0200

    Add files via upload

---
+++ deposit-images/header.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Socienet</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- enter content after publishing -->
+    <meta property="og:title" content="Socienet">
+    <meta property="og:type" content="Technology">
+    <meta property="og:url" content="https://socienet.w3spaces.com">
+    <meta property="og:image" content="">
+
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="apple-touch-icon" href="icon.png">
+    <!-- Place favicon.ico in the root directory -->
+    <!-- fontawesome -->
+    <script src="https://kit.fontawesome.com/ccbd4d6bc1.js" crossorigin="anonymous"></script>
+    <!-- Google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet">
+    <!-- end of google fonts -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="/css/style.css">
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <meta name="theme-color" content="#fafafa">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <div class="container">
+                <div>
+
+                    <img src="img/logo/socienet-white.svg" alt="logo" class="logo">
+
+
+                    <div class="menu">
+                        <ul>
+                            <li>
+                                <a href="index.html" class="active">Home</a>
+                            </li>
+                            <li>
+                                <a href="projects.html">Projects</a>
+                            </li>
+                            <li>
+                                <a href="contact.html">Contact Us</a>
+                            </li>
+                            <li>
+                                <a href="about-us.html">About Us</a>
+                            </li>
+                        </ul>
+                        <ul class="social-media">
+                            <li>
+                                <a href="#" class="mediaicon"><i class="fab fa-instagram"></i></a>
+                            </li>
+                            <li>
+                                <a href="#" class="mediaicon"><i class="fab fa-facebook"></i></a>
+                            </li>
+                            <li>
+                                <a href="#" class="mediaicon"><i class="fab fa-twitter"></i></a>
+                            </li>
+                            <li>
+                                <a href="#" class="mediaicon"><i class="fab fa-youtube"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="menu-button">
+                    <i class="fas fa-bars"></i>
+                </div>
+            </div>
+        </nav>
---
+++ deposit-images/index.php
@@ -0,0 +1,92 @@
+<?php
+include('header.php');
+?>
+
+<div class="content" data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="1000">
+    <div>
+        <small>Jusuf Abdullahu</small>
+        <h1>Full Stack Developer</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat. Duis
+            aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+            sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <a href="#">About</a>
+    </div>
+
+</div>
+</header>
+
+<section class="projects" data-aos="fade-up">
+    <div class="overlay">
+        <div>
+            <h1>Our Projects</h1>
+            <a href="#">Explore</a>
+        </div>
+    </div>
+    <img src="img/1.jpg" alt="Projects">
+    <img src="img/3.jpg" alt="Projects">
+    <img src="img/4.jpg" alt='Projects' class="hide ">
+    <img src="img/img.jpg " alt="Projects " class="hide ">
+</section>
+
+<section class="posts ">
+    <div class="row " data-aos="fade-up ">
+        <img src="img/img-back.jpg " alt="Projects ">
+        <div>
+            <small>Jusuf Abdullahu</small>
+            <h1>Web Developer</h1>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+                scrambled it to make a type specimen
+                book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining
+                essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+                Lorem Ipsum passages, and
+                more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+            <a href="# ">Explore</a>
+        </div>
+    </div>
+    <div class="row " data-aos="flip-left">
+        <img src="img/3.jpg " alt="Projects ">
+        <div>
+            <small>Jusuf Abdullahu</small>
+            <h1>Web Developer</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum.</p>
+            <a href="# ">Explore</a>
+        </div>
+    </div>
+    <div class="row " data-aos="zoom-in-up">
+        <img src="img/4.jpg " alt="Projects ">
+        <div>
+            <small>Jusuf Abdullahu</small>
+            <h1>Web Developer</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum.</p>
+            <a href="# ">Explore</a>
+        </div>
+    </div>
+    <div class="row" data-aos="fade-left ">
+        <img src="img/img.jpg " alt="Projects ">
+        <div>
+            <small>Jusuf Abdullahu</small>
+            <h1>Web Developer</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum.</p>
+            <a href="# ">Explore</a>
+        </div>
+    </div>
+</section>
+<?php
+include("footer.php")
+?>
